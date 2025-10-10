44037b326e8cae6a8a177c2ee876e8c07cb2a27a
varraschin <marcelojuniber@gmail.com>
2025-10-10 20:33:07 -0300

    Criado os serviços de conexão

--- GCook/Data/AppDbContext.cs
+++ GCook/Data/AppDbContext.cs
@@ -11,5 +11,18 @@ namespace GCook.Data
         }
 
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Ingrediente> Ingredientes { get; set; }
+        public DbSet<Receita> Receitas { get; set; }
+        public DbSet<ReceitaIngrediente> ReceitaIngredientes { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ReceitaIngrediente>()
+                .HasKey(ri => new {ri.ReceitaId, ri.IngredienteId});
+        }
     }
 }
